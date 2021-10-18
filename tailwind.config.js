3d082b19515eee6d01ba64e7c4aae38db26f9b90
Mauro Mottini <mottinimauro@gmail.com>
2021-10-18 00:16:58 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      black: '#2e2e2e',
+      white: '#d6d6d6',
+      pink: '#b05279',
+      blue: '#6c99bb',
+      yellow: '#e5b567',
+      green:  '#b4d273',
+      orange: '#e87d3e',
+      purple: '#9e86c8'
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
